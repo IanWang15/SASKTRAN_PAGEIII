fd1721dc33da3ca91e0758e803bef5f4b9c4aa58
Yi Wang <52504365+IanWang15@users.noreply.github.com>
2022-03-22 23:47:13 -0500

    Update locPkg.py

--- locPkg.py
+++ locPkg.py
@@ -3,3 +3,9 @@
 import sasktran as sk
 import imp
 print(imp.find_module('sasktran'))
+
+# used for finding a global configuration file contains some settings, particularly the location of large databases
+
+print(sk.config.user_config_file_location())
+
+
